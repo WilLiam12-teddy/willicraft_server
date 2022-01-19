6bf87a10d30e8459205e4053e409dea64c3d26a5
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2022-01-20 09:15:18 +1400

    Add files via upload

---
+++ extra_hiper/init.lua
@@ -0,0 +1,72 @@
+local pictures = {  ["extra"]="Extra Hiper",
+                    ["maisbarato"]="Mais Barato",
+                    ["extra2"]="Extra",
+                    ["atacado"]="Atacado",
+                    ["bemvindo"]="Bem-vindo!",
+                    ["padaria"]="Padaria",
+                    ["maisbarato2"]="Mais Barato !",
+                    ["extracombr"]="Loja online"
+
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("extra_hiper:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 3.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
+
+local pictures = {  ["extracombr_pequeno"]="Extra . com . br"
+
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("extra_hiper:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 1.1,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
