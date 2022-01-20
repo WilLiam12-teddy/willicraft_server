742933aa37cbe4023cb9fdfe2470cbfb1825ec20
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2022-01-21 08:59:11 +1400

    Update init.lua

--- extra_hiper/init.lua
+++ extra_hiper/init.lua
@@ -70,3 +70,36 @@ for name,desc in pairs(pictures) do
         end,
     })
 end
+
+local pictures = {  ["extra_grande"]="Extra = Mais Barato, Mais Barato."
+
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("extra_hiper:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 4.7,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
